ab9197fa5c75ecba184132dad1280aadcb95df4c
Maxim Velikoselskih <maxv@eljur.team>
2021-12-22 16:47:16 +0700

    list product in process

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+
+class ProductController extends Controller
+{
+    public function list(int $categoryId)
+    {
+        $category = Category::findOrFail($categoryId);
+        $query = \DB::table('product_variants');
+        $query
+            ->select([
+                'product_variants.id as id',
+                'product_variants.code as code',
+            ])
+            ->leftJoin('products', 'product_variants.product_id', 'products.id');
+
+        $numAlias = 1;
+        $aliasFields = 'f' . $numAlias;
+        $aliasCategory = 'c' . $numAlias;
+        $aliasFieldValue = 'fv' . $numAlias;
+
+        $query->leftJoin(
+            "categories as $aliasCategory",
+            "$aliasCategory.id",
+            'products.category_id'
+        );
+
+        $query->leftJoin(
+            "fields as $aliasFields",
+            "$aliasFields.category_id",
+            "$aliasCategory.id"
+        );
+
+        $query->leftJoin(
+            "field_values as $aliasFieldValue",
+            "$aliasFieldValue.field_id",
+            "$aliasFields.id"
+        );
+
+        $query->addSelect([
+            "$aliasFields.code as $aliasFields" . '_code',
+            "$aliasFieldValue.value as $aliasFieldValue" . '_value',
+        ]);
+
+        while ($category = $category->parent) {
+            $numAlias++;
+            $aliasFields = 'f' . $numAlias;
+            $parentAliasCategory = 'c' . $numAlias;
+            $aliasFieldValue = 'fv' . $numAlias;
+
+            $query->leftJoin(
+                "categories as $parentAliasCategory",
+                "$parentAliasCategory.id",
+                "$aliasCategory.parent_id"
+            );
+
+            $query->leftJoin(
+                "fields as $aliasFields",
+                "$aliasFields.category_id",
+                "$parentAliasCategory.id"
+            );
+
+            $query->leftJoin(
+                "field_values as $aliasFieldValue",
+                "$aliasFieldValue.field_id",
+                "$aliasFields.id"
+            );
+
+            $query->addSelect([
+                "$aliasFields.code as $aliasFields" . '_code',
+                "$aliasFieldValue.value as $aliasFieldValue" . '_value',
+            ]);
+
+            $aliasCategory = $parentAliasCategory;
+        }
+
+        return $query->get();
+    }
+}
